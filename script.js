38d2d641ee7b6acc76f2756924b762f83d3ca586
Marshal10 <marshalmaleane123@gmail.com>
2024-04-30 20:16:32 +0530

    Mouse leave animation

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const menu=document.querySelector('.menu')
 centerEl.addEventListener('mouseover',()=>{
     menu.classList.add("change")
 })
+
+menu.addEventListener('mouseleave',()=>{
+    menu.classList.remove("change")
+})
